3ba99ef8973c4f38597d1151904e13bf43035d5c
trannhon2509 <nhonvn2509@gmail.com>
2024-09-08 06:38:22 +0700

    Refactor route rendering in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,28 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { publishRoutes, privateRoutes } from './routes/AppRoute'
+import { publishRoutes, privateRoutes } from './routes/AppRoute';
 
 function App() {
   const renderRoutes = (routes) => {
-    return routes.map(
-      ({ path, component: Component, layout: Layout }, index) => {
-        if (Layout) {
-          return (
-            <Route
-              key={index}
-              path={path}
-              element={
-                <Layout>
-                  <Component />
-                </Layout>
-              }
-            />
-          );
-        }
-        return <Route key={index} path={path} element={<Component />} />;
+    return routes.map(({ path, component: Component, layout: Layout }, index) => {
+      if (Layout) {
+        return (
+          <Route
+            key={index}
+            path={path}
+            element={
+              <Layout>
+                <Component />
+              </Layout>
+            }
+          />
+        );
       }
-    );
+      return <Route key={index} path={path} element={<Component />} />;
+    });
   };
 
   return (
-    <Router basename="/Travel-Mate-FrontEnd">
+    <Router basename="/Travel-Mate-FrontEnd"> {/* Ensure this matches your repo name */}
       <Routes>
         {renderRoutes(publishRoutes)}
         {renderRoutes(privateRoutes)}
