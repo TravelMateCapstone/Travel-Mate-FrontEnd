3b81c54a270a5ebdc720ebf953d1e2fd53fc10bc
trannhon2509 <nhonvn2509@gmail.com>
2024-09-08 10:19:30 +0700

    Update routing configuration and add navigation to settings in Dashboard component

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,22 @@
 import React from "react";
-import Couter from '../components/Shared/Couter'
-
+import { useNavigate } from "react-router-dom";
+import Couter from '../components/Shared/Couter';
 
 function Dashboard() {
+  const navigate = useNavigate(); // Sử dụng useNavigate để điều hướng
+
+  const handleNavigateToSettings = () => {
+    navigate("/setting"); // Điều hướng đến trang /settings
+  };
+
   return (
     <div>
       <h1>Dashboard</h1>
       <Couter />
+      {/* Nút để điều hướng đến trang settings */}
+      <button onClick={handleNavigateToSettings}>
+        Go to Settings
+      </button>
     </div>
   );
 }
