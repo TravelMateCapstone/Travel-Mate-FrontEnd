ba531baae10d916256d331795cf5fb6f4e107ca7
trannhon2509 <nhonvn2509@gmail.com>
2024-09-08 20:02:07 +0700

    Fix respone sive footer

--- src/components/Shared/Footer.jsx
+++ src/components/Shared/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
       <div className="new_footer_top">
         <div className="container-fluid">
           <div className="row">
-            <div className="col-lg-3 col-md-6">
+            <div className="col-lg-3 col-md-12 col-sm-12">
               <div className="f_widget company_widget wow fadeInLeft" data-wow-delay="0.2s" style={{ visibility: 'visible', animationDelay: '0.2s', animationName: 'fadeInLeft' }}>
                 <h3 className="f-title f_600 t_color f_size_18">Get in Touch</h3>
                 <p>Don’t miss any updates of our new templates and extensions!</p>
@@ -19,39 +19,40 @@ const Footer = () => {
                 </form>
               </div>
             </div>
-            <div className="col-lg-3 col-md-6">
-              <div className="f_widget about-widget pl_70 wow fadeInLeft" data-wow-delay="0.4s" style={{ visibility: 'visible', animationDelay: '0.4s', animationName: 'fadeInLeft' }}>
-                <h3 className="f-title f_600 t_color f_size_18">Download</h3>
-                <ul className="list-unstyled f_list">
-                  <li><a href="#">Company</a></li>
-                  <li><a href="#">Android App</a></li>
-                  <li><a href="#">Ios App</a></li>
-                </ul>
-              </div>
-            </div>
-            <div className="col-lg-3 col-md-6">
-              <div className="f_widget about-widget pl_70 wow fadeInLeft" data-wow-delay="0.6s" style={{ visibility: 'visible', animationDelay: '0.6s', animationName: 'fadeInLeft' }}>
-                <h3 className="f-title f_600 t_color f_size_18">Help</h3>
-                <ul className="list-unstyled f_list">
-                  <li><a href="#">FAQ</a></li>
-                  <li><a href="#">Term &amp; conditions</a></li>
-                  <li><a href="#">Support</a></li>
-                  <li><a href="#">Blog</a></li>
-                  <li><a href="#">Privacy</a></li>
-                </ul>
-              </div>
-            </div>
-            <div className="col-lg-3 col-md-6">
-              <div className="f_widget social-widget pl_70 wow fadeInLeft" data-wow-delay="0.8s" style={{ visibility: 'visible', animationDelay: '0.8s', animationName: 'fadeInLeft' }}>
-                <h3 className="f-title f_600 t_color f_size_18">Team Solutions</h3>
-                <div className="f_social_icon">
-                  <a href="#" className="bi bi-facebook" />
-                  <a href="#" className="bi bi-twitter" />
-                  <a href="#" className="bi bi-linkedin" />
-                  <a href="#" className="bi bi-pinterest" />
+
+            {/* Align Download, Help, and Team Solutions on the same row for tablet (md) screens */}
+            <div className="col-lg-6 col-md-12 col-sm-12 mt-3">
+              <div className="row">
+                <div className="f_widget col-6 col-md-4 col-sm-6 about-widget wow fadeInLeft" data-wow-delay="0.6s" style={{ visibility: 'visible', animationDelay: '0.6s', animationName: 'fadeInLeft' }}>
+                  <h3 className="f-title f_600 t_color f_size_18">Download</h3>
+                  <ul className="list-unstyled f_list">
+                    <li><a href="#">Android App</a></li>
+                    <li><a href="#">iOS App</a></li>
+                    <li><a href="#">Documents</a></li>
+                  </ul>
+                </div> 
+                <div className="f_widget col-6 col-md-4 col-sm-6 about-widget wow fadeInLeft" data-wow-delay="0.6s" style={{ visibility: 'visible', animationDelay: '0.6s', animationName: 'fadeInLeft' }}>
+                  <h3 className="f-title f_600 t_color f_size_18">Help</h3>
+                  <ul className="list-unstyled f_list">
+                    <li><a href="#">FAQ</a></li>
+                    <li><a href="#">Terms & Conditions</a></li>
+                    <li><a href="#">Support</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Privacy</a></li>
+                  </ul>
+                </div>
+                <div className="f_widget col-12 col-md-4 social-widget wow fadeInLeft" data-wow-delay="0.8s" style={{ visibility: 'visible', animationDelay: '0.8s', animationName: 'fadeInLeft' }}>
+                  <h3 className="f-title f_600 t_color f_size_18">Team Solutions</h3>
+                  <div className="f_social_icon d-flex">
+                    <a href="#" className="bi bi-facebook" />
+                    <a href="#" className="bi bi-twitter" />
+                    <a href="#" className="bi bi-linkedin" />
+                    <a href="#" className="bi bi-pinterest" />
+                  </div>
                 </div>
               </div>
             </div>
+
           </div>
         </div>
         <div className="footer_bg">
@@ -59,7 +60,6 @@ const Footer = () => {
           <div className="footer_bg_two" />
         </div>
       </div>
-      
     </footer>
   );
 };
