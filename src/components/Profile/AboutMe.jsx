6d22d7894be8208fbe5113072a4b3bdf11ebd677
LeenTD <trandanglen2002@gmail.com>
2024-09-19 02:20:52 +0700

    fe profile v2

--- src/components/Profile/AboutMe.jsx
+++ src/components/Profile/AboutMe.jsx
@@ -4,10 +4,10 @@ import { Card, Container } from 'react-bootstrap';
 function AboutMe() {
   return (
     <Container className='p-3' style={{ background: '#F3F6FF' }}>
-      <h1 className="mb-4">About Me</h1>
+      <h1 className="mb-4 text-success"><i class="bi bi-info-circle"></i> GIỚI THIỆU</h1>
 
       <Card className="mb-3" style={{ backgroundColor: '#E8F0FE' }}>
-        <Card.Header as="h5"><i className="bi bi-circle"></i> INTRODUCE</Card.Header>
+        <Card.Header as="h5"><i className="bi bi-circle"></i> MÔ TẢ</Card.Header>
         <Card.Body>
           <Card.Text className="small ms-5">
             Hi, I'm a passionate developer with a love for building web and mobile applications.
@@ -16,7 +16,7 @@ function AboutMe() {
       </Card>
 
       <Card className="mb-3" style={{ backgroundColor: '#E8F0FE' }}>
-        <Card.Header as="h5"><i className="bi bi-circle"></i> WHY I'M HERE</Card.Header>
+        <Card.Header as="h5"><i className="bi bi-circle"></i> TẠI SAO TÔI SỬ DỤNG TRAVEL MATE</Card.Header>
         <Card.Body>
           <Card.Text className="small ms-5">
             I’m here to expand my knowledge, collaborate on exciting projects, and share my skills.
@@ -25,7 +25,7 @@ function AboutMe() {
       </Card>
 
       <Card className="mb-3" style={{ backgroundColor: '#E8F0FE' }}>
-        <Card.Header as="h5"><i className="bi bi-circle"></i> INTERESTS</Card.Header>
+        <Card.Header as="h5"><i className="bi bi-circle"></i> SỞ THÍCH</Card.Header>
         <Card.Body>
           <Card.Text className="small ms-5">
             I love technology, coding, reading about AI, and exploring new tools and frameworks.
@@ -34,7 +34,7 @@ function AboutMe() {
       </Card>
 
       <Card className="mb-3" style={{ backgroundColor: '#E8F0FE' }}>
-        <Card.Header as="h5"><i className="bi bi-circle"></i> MUSIC</Card.Header>
+        <Card.Header as="h5"><i className="bi bi-circle"></i> ÂM NHẠC, PHIM ẢNH & SÁCH</Card.Header>
         <Card.Body>
           <Card.Text className="small ms-5">
             I enjoy listening to classical music, as well as modern indie and electronic genres.
@@ -43,7 +43,7 @@ function AboutMe() {
       </Card>
 
       <Card className="mb-3" style={{ backgroundColor: '#E8F0FE' }}>
-        <Card.Header as="h5"><i className="bi bi-circle"></i> MOVIES & BOOKS</Card.Header>
+        <Card.Header as="h5"><i className="bi bi-circle"></i> TÔI CÓ THỂ CHIA SẺ GÌ VỚI BẠN</Card.Header>
         <Card.Body>
           <Card.Text className="small ms-5">
             I’m a big fan of sci-fi movies and love reading books that challenge my thinking.
@@ -52,7 +52,7 @@ function AboutMe() {
       </Card>
 
       <Card className="mb-3" style={{ backgroundColor: '#E8F0FE' }}>
-        <Card.Header as="h5"><i className="bi bi-circle"></i> WHAT I CAN SHARE</Card.Header>
+        <Card.Header as="h5"><i className="bi bi-circle"></i> TÔI CÓ THỂ CHIA SẺ GÌ VỚI BẠN</Card.Header>
         <Card.Body>
           <Card.Text className="small ms-5">
             I can share my knowledge about web development, mobile app development, and experience with APIs.
--- src/components/Profile/MyHome.jsx
+++ src/components/Profile/MyHome.jsx
@@ -3,46 +3,54 @@ import { Card, Container } from 'react-bootstrap';
 
 function MyHome() {
   return (
-    <Container className='p-3 vh-100' style={{ background: '#F3F6FF' }}>
-      <h1 className=" mb-4"><i class="bi bi-house-door"></i> Dang's Home</h1>
+    <Container className='p-3' style={{ background: '#F3F6FF' }}>
+      <h1 className=" mb-4 text-success"><i class="bi bi-house-door"></i> NHÀ CỦA TÔI</h1>
 
       <Card className="mb-3">
-        <Card.Header as="h5">DANG’S PREFERENCES</Card.Header>
+        <Card.Header as="h5">ƯU TIÊN</Card.Header>
         <Card.Body>
           <Card.Text className="small ms-3">
-            <i class="bi bi-person-check-fill"></i> Max number of Guests: Any
+            <i class="bi bi-person-check-fill"></i> Số lượng khách tối đa: 2
           </Card.Text>
           <Card.Text className="small ms-3">
-            Smoking Allowed?  Yes
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gender-trans" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M0 .5A.5.5 0 0 1 .5 0h3a.5.5 0 0 1 0 1H1.707L3.5 2.793l.646-.647a.5.5 0 1 1 .708.708l-.647.646.822.822A4 4 0 0 1 8 3c1.18 0 2.239.51 2.971 1.322L14.293 1H11.5a.5.5 0 0 1 0-1h4a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V1.707l-3.45 3.45A4 4 0 0 1 8.5 10.97V13H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V14H6a.5.5 0 0 1 0-1h1.5v-2.03a4 4 0 0 1-3.05-5.814l-.95-.949-.646.647a.5.5 0 1 1-.708-.708l.647-.646L1 1.707V3.5a.5.5 0 0 1-1 0zm5.49 4.856a3 3 0 1 0 5.02 3.288 3 3 0 0 0-5.02-3.288" />
+            </svg> Giới tính tôi muốn đón: Bất kỳ
           </Card.Text>
-        </Card.Body>
-      </Card>
-
-      <Card className="mb-3">
-        <Card.Header as="h5">SLEEPING ARRANGEMENTS</Card.Header>
-        <Card.Body>
           <Card.Text className="small ms-3">
-            Shared room
+            <i class="bi bi-ban"></i> Cho phép hút thuốc? Có
           </Card.Text>
         </Card.Body>
       </Card>
 
       <Card className="mb-3">
-        <Card.Header as="h5">MORE DETAILS</Card.Header>
+        <Card.Header as="h5">NƠI TÁ TÚC</Card.Header>
         <Card.Body>
-          <Card.Header as="h6">Roommate Situation</Card.Header>
+          <Card.Text className="small ms-3">
+            <i class="bi bi-door-open"></i> Phòng đơn
+          </Card.Text>
+          <Card.Text className="small ms-3">
+            Sofa in the living room or private room
+          </Card.Text>
+        </Card.Body>
+      </Card>
+
+      <Card className="mb-3">
+        <Card.Header as="h5">CHI TIẾT</Card.Header>
+        <Card.Body>
+          <Card.Header as="h6"> <i class="bi bi-people-fill"></i> Bạn cùng phòng</Card.Header>
           <Card.Text className="small ms-3">
             I live alone
           </Card.Text>
-          <Card.Header as="h6">What I Can Share with Guests</Card.Header>
+          <Card.Header as="h6"> <i class="bi bi-link-45deg"></i> Tôi có thể chia sẽ gì với bạn ?</Card.Header>
           <Card.Text className="small ms-3">
             A place to sleep, free kitchen, free bathroom, wifi, my time and accompany
           </Card.Text>
-          <Card.Header as="h6">Public Transportation Access</Card.Header>
+          <Card.Header as="h6"> <i class="bi bi-car-front"></i> Phương tiện di chuyển</Card.Header>
           <Card.Text className="small ms-3">
             I have a bike
           </Card.Text>
-          <Card.Header as="h6">Additional Info</Card.Header>
+          <Card.Header as="h6"><i class="bi bi-info-circle"></i> Thông tin bổ sung</Card.Header>
           <Card.Text className="small ms-3">
             Feel free to ask me anything. Welcome!
           </Card.Text>
