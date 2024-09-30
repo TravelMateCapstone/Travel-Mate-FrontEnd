f877a0d9507ff4cbfa102cd4cdd1ece4ad09b5ef
dungnguyen <dungnttde160057@fpt.edu.vn>
2024-09-30 20:49:10 +0700

    Profile Management

--- src/pages/EditMyHome.jsx
+++ src/pages/EditMyHome.jsx
@@ -7,7 +7,7 @@ const MyHome = () => {
     <div className="p-4 edit-pro-container-myhome">
       <Form>
         {/* Title */}
-        <div className="d-flex justify-content-start mb-4">
+        <div className="d-flex justify-content-start mb-4 ">
           <h5 className="text-success me-4">GIỚI THIỆU</h5>
           <h5>NHÀ CỦA TÔI</h5>
         </div>
@@ -16,20 +16,20 @@ const MyHome = () => {
         <Row className="mb-3 align-items-start" style={{ position: 'relative' }}>
           {/* Left Section */}
           <Col md={5}>
-            <h5 className="text-danger">ƯU TIÊN</h5>
+            <h5 className="text-danger fw-medium">ƯU TIÊN</h5>
             <Form.Group controlId="maxGuests" className="mb-3">
-              <Form.Label>Số lượng khách tối đa</Form.Label>
-              <Form.Control type="text" placeholder="" className='label-small-form-myhome'/>
+              <Form.Label className='fw-medium'>Số lượng khách tối đa</Form.Label>
+              <Form.Control type="text" placeholder="10"  className='label-small-form-myhome' />
             </Form.Group>
 
             <Form.Group controlId="genderPreference" className="mb-3">
-              <Form.Label>Giới tính tôi muốn đón</Form.Label>
+              <Form.Label className='fw-medium'>Giới tính tôi muốn đón</Form.Label>
               <Form.Control type="text" placeholder="" className='label-small-form-myhome' />
             </Form.Group>
 
             <Form.Group controlId="smokingAllowed" className="mb-3">
-              <Form.Label>Cho phép hút thuốc</Form.Label>
-              <Form.Control type="text" placeholder="" className='label-small-form-myhome'/>
+              <Form.Label className='fw-medium'>Cho phép hút thuốc</Form.Label>
+              <Form.Control type="text" placeholder="" className='label-small-form-myhome' />
             </Form.Group>
           </Col>
 
@@ -42,12 +42,12 @@ const MyHome = () => {
           <Col md={6}>
             <h5 className="text-danger">NƠI TÁ TÚC</h5>
             <Form.Group controlId="roomType" className="mb-3">
-              <Form.Label>Loại phòng</Form.Label>
+              <Form.Label className='fw-medium'>Loại phòng</Form.Label>
               <Form.Control type="text" placeholder="" className='label-small-form-myhome' />
             </Form.Group>
 
             <Form.Group controlId="roomDescription" className="mb-3">
-              <Form.Label>Mô tả phòng</Form.Label>
+              <Form.Label className='fw-medium'>Mô tả phòng</Form.Label>
               <Form.Control as="textarea" rows={3} placeholder="" className='text-form-myhome-2' />
             </Form.Group>
           </Col>
@@ -57,48 +57,72 @@ const MyHome = () => {
         <hr />
 
         {/* Additional Form Fields (Vertical Layout) */}
-        <h5 className="text-danger">CHI TIẾT</h5>
+        <h5 className="text-danger fw-medium">CHI TIẾT</h5>
         <Row>
           <Col md={12}>
             <Form.Group controlId="roommate" className="mb-3">
-              <Form.Label>Bạn cùng phòng</Form.Label>
-              <Form.Control type="text" placeholder="" className='label-small-form-myhome'/>
+              <Form.Label className='fw-medium'>Bạn cùng phòng</Form.Label>
+              <Form.Control type="text" placeholder="" className='label-small-form-myhome' />
             </Form.Group>
 
             <Form.Group controlId="sharedItems" className="mb-3">
-              <Form.Label className=''>Tôi có thể chia sẻ gì với bạn</Form.Label>
+              <Form.Label className='fw-medium'>Tôi có thể chia sẻ gì với bạn</Form.Label>
               <Form.Control as="textarea" rows={3} placeholder="" className='text-form-myhome' />
             </Form.Group>
 
             <Form.Group controlId="transportation" className="mb-3">
-              <Form.Label>Phương tiện di chuyển</Form.Label>
-              <Form.Control type="text" placeholder="" className='label-small-form-myhome'/>
+              <Form.Label className='fw-medium'>Phương tiện di chuyển</Form.Label>
+              <Form.Control type="text" placeholder="" className='label-small-form-myhome' />
             </Form.Group>
 
             <Form.Group controlId="additionalInfo" className="mb-3">
-              <Form.Label className=''>Thông tin bổ sung</Form.Label>
-              <Form.Control as="textarea" rows={3} placeholder="" className='text-form-myhome'/>
+              <Form.Label className='fw-medium'>Thông tin bổ sung</Form.Label>
+              <Form.Control as="textarea" rows={3} placeholder="" className='text-form-myhome' />
             </Form.Group>
           </Col>
         </Row>
-    {/* Image Section */}
-    <div className='display-form-myhome'>
-    <h5 className="mt-4">Hình ảnh nhà của bạn</h5>
-    <div className="d-flex justify-content-start mb-3">
-              <button className="btn btn-outline-primary">Nhấn vào đây để upload</button>
-    </div>
-    </div>
-    <div className="image-grid-container display-form-myhome">
-  <img src="https://divui.com/blog/wp-content/uploads/2018/10/111111.jpg" alt="Image 1" className="img-thumbnail" />
-  <img src="https://divui.com/blog/wp-content/uploads/2018/10/111111.jpg" alt="Image 2" className="img-thumbnail" />
-  <img src="https://divui.com/blog/wp-content/uploads/2018/10/111111.jpg" alt="Image 3" className="img-thumbnail" />
-  
- </div>
- <div className="image-grid-container display-form-myhome">
-  <img src="https://divui.com/blog/wp-content/uploads/2018/10/111111.jpg" alt="Image 1" className="img-thumbnail" />
-  <img src="https://divui.com/blog/wp-content/uploads/2018/10/111111.jpg" alt="Image 2" className="img-thumbnail" />
-  <img src="https://divui.com/blog/wp-content/uploads/2018/10/111111.jpg" alt="Image 3" className="img-thumbnail" />
-</div>
+        {/* Image Section */}
+        <div className='display-form-myhome mt-4' >
+          <h5 className=" fw-medium image-tiltle">Hình ảnh nhà của bạn</h5>
+          <div className="d-flex justify-content-start">
+            <button className="btn btn-outline-primary">Nhấn vào đây để upload</button>
+          </div>
+        </div>
+        <div className="container">
+          <div className='row'>
+          <div className='col col-lg-4 col-md-6 col-6  image-grid-container'>
+            <div className='img-thumbnail'>
+            <img src='https://vcdn1-dulich.vnecdn.net/2022/06/03/cauvang-1654247842-9403-1654247849.jpg?w=1200&h=0&q=100&dpr=1&fit=crop&s=Swd6JjpStebEzT6WARcoOA' alt='image-profile'></img>
+            </div>
+          </div>
+          <div className='col col-lg-4  col-md-6 col-6  image-grid-container'>
+            <div className='img-thumbnail'>
+            <img src='https://vcdn1-dulich.vnecdn.net/2022/06/03/cauvang-1654247842-9403-1654247849.jpg?w=1200&h=0&q=100&dpr=1&fit=crop&s=Swd6JjpStebEzT6WARcoOA' alt='image-profile'></img>
+            </div>
+          </div>
+          <div className='col col-lg-4 col-md-6 col-6 image-grid-container'>
+            <div className='img-thumbnail'>
+            <img src='https://vcdn1-dulich.vnecdn.net/2022/06/03/cauvang-1654247842-9403-1654247849.jpg?w=1200&h=0&q=100&dpr=1&fit=crop&s=Swd6JjpStebEzT6WARcoOA' alt='image-profile'></img>
+            </div>
+          </div>
+          <div className='col col-lg-4 col-md-6 col-6 image-grid-container'>
+            <div className='img-thumbnail'>
+            <img src='https://vcdn1-dulich.vnecdn.net/2022/06/03/cauvang-1654247842-9403-1654247849.jpg?w=1200&h=0&q=100&dpr=1&fit=crop&s=Swd6JjpStebEzT6WARcoOA' alt='image-profile'></img>
+            </div>
+          </div>
+          <div className='col col-lg-4 col-md-6 col-6 image-grid-container'>
+            <div className='img-thumbnail'>
+            <img src='https://vcdn1-dulich.vnecdn.net/2022/06/03/cauvang-1654247842-9403-1654247849.jpg?w=1200&h=0&q=100&dpr=1&fit=crop&s=Swd6JjpStebEzT6WARcoOA' alt='image-profile'></img>
+            </div>
+          </div>
+          <div className='col col-lg-4 col-md-6 col-6 image-grid-container'>
+            <div className='img-thumbnail'>
+            <img src='https://vcdn1-dulich.vnecdn.net/2022/06/03/cauvang-1654247842-9403-1654247849.jpg?w=1200&h=0&q=100&dpr=1&fit=crop&s=Swd6JjpStebEzT6WARcoOA' alt='image-profile'></img>
+            </div>
+          </div>
+          </div>
+        </div>
+
 
         {/* Save/Cancel Buttons */}
         <div className="d-flex justify-content-end mt-4">
