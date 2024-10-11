267eb1507d1d811dd24245bf604eb27a8a4912c1
nguyenthithuydung2104 <dungnttde160057@fpt.edu.vn>
2024-10-11 21:14:28 +0700

    fix logic searc list propose

--- src/components/SearchList/ProposeSearchListCard.jsx
+++ src/components/SearchList/ProposeSearchListCard.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import SearchListCard from './SearchListCard'
 import { Col, Row } from 'react-bootstrap';
+import { useLocation } from 'react-router-dom';
+import RoutePath from '../../routes/RoutePath';
 
 function ProposeSearchListCard() {
     // Dữ liệu ví dụ cho các thẻ (giả định rằng bạn có sẵn một array dữ liệu)
@@ -22,16 +24,39 @@ function ProposeSearchListCard() {
         // Add more data here...
     ];
 
+    const currentItemsTraveller = [
+        {
+            id: 1,
+            title: "Card Title traveller",
+            connections: 35,
+            description: "Went from a computer science intern to the head of a $2 billion company.",
+            imageUrl: "https://yt3.googleusercontent.com/oN0p3-PD3HUzn2KbMm4fVhvRrKtJhodGlwocI184BBSpybcQIphSeh3Z0i7WBgTq7e12yKxb=s900-c-k-c0x00ffffff-no-rj"
+        },
+        {
+            id: 2,
+            title: "Card Title 2",
+            connections: 40,
+            description: "Has worked in the tech industry for over a decade.",
+            imageUrl: "https://yt3.googleusercontent.com/oN0p3-PD3HUzn2KbMm4fVhvRrKtJhodGlwocI184BBSpybcQIphSeh3Z0i7WBgTq7e12yKxb=s900-c-k-c0x00ffffff-no-rj"
+        },
+    ];
+
+    const location = useLocation();
+    let itemsToDisplay = currentItems;
+    if (location.pathname === RoutePath.SEARCHLISTTRAVELLER) {
+        itemsToDisplay = currentItemsTraveller;
+    }
+
     return (
         <div className='d-flex flex-column'>
             <Row className='m-0 p-0'>
-                {currentItems.map((item) => (
+                {itemsToDisplay.map((item) => (
                     <SearchListCard
-                    title={item.title}
-                    connections={item.connections}
-                    description={item.description}
-                    imageUrl={item.imageUrl}
-                />
+                        title={item.title}
+                        connections={item.connections}
+                        description={item.description}
+                        imageUrl={item.imageUrl}
+                    />
                 ))}
             </Row>
         </div>
