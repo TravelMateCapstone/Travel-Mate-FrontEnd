592dbdb42c99dbbcc309a714980271276684cbae
Nhơn Trần <nhonvn2509@gmail.com>
2024-10-14 18:13:59 +0700

    Refactor vite.config.js to dynamically set the base path for production and development environments

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/Travel-Mate-FrontEnd/', 
-  test: {
-    environment: 'jsdom',  // Sử dụng jsdom để mô phỏng DOM cho test
-    globals: true,         // Cho phép sử dụng các biến toàn cục như 'describe', 'test', 'expect'
-    setupFiles: './src/setupTests.js',  // Đường dẫn file setup cho test (nếu cần)
-  },
- 
-})
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    base: mode === 'production' ? '/' : '/Travel-Mate-FrontEnd/',  // Dùng '/' cho production, '/Travel-Mate-FrontEnd/' cho development
+    test: {
+      environment: 'jsdom',  // Sử dụng jsdom để mô phỏng DOM cho test
+      globals: true,         // Cho phép sử dụng các biến toàn cục như 'describe', 'test', 'expect'
+      setupFiles: './src/setupTests.js',  // Đường dẫn file setup cho test (nếu cần)
+    },
+  };
+});
