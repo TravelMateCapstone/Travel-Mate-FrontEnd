d070b740ac230503a6b1a24f244c5ef82adbd7c3
Nhơn Trần <nhonvn2509@gmail.com>
2024-10-15 22:33:28 +0700

    Refactor vite.config.js: remove commented out base URL

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Travel-Mate-FrontEnd/', 
+  // base: '/Travel-Mate-FrontEnd/', 
   test: {
     environment: 'jsdom',  // Sử dụng jsdom để mô phỏng DOM cho test
     globals: true,         // Cho phép sử dụng các biến toàn cục như 'describe', 'test', 'expect'
